9ba96e4e9dab8f87a5e17f76f24971b7584ffa9a
tiagoo <thurstaltamirano@escuelasproa.edu.ar>
2023-04-17 15:24:40 -0300

    cambios

--- script.js
+++ script.js
@@ -1,29 +1,24 @@
+
+
 const botones = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "C", "/", "X", "+", "-","="];
 let num1 = [];
 let num2 = [];
 let operacionIngresada = false; 
+let input = document.getElementById("result")
 
 
 
 for (let i = 0; i < botones.length; i++) {
   const boton = document.getElementById(botones[i]);
-  boton.addEventListener("click", function() {
-    if (botones[i] === "+" || botones[i] === "-" || botones[i] === "X" || botones[i] === "/" ||  botones[i] === "C") {    
-      console.log("ingresa un numero");
-      num1 += botones[i];
-      operacionIngresada = true;
-    } else {
-      if (operacionIngresada) {
-        num2 += botones[i];
-        console.log("el num2 es " + num2);
-      } 
-    }
-    if (botones[i] === "=") {
-      
-        console.log(num1+=num2)
-      
-    }
+  boton.addEventListener("click", function () {
+    if (botones[i] === "C")
+    { input.value = "" }
     
-    console.log("Hiciste clic en el botón " + botones[i]);
+    if (botones[i] === "1" || botones[i] === "2" || botones[i] === "3" || botones[i] === "4" || botones[i] === "5" || botones[i] === "6"
+      || botones[i] === "7" || botones[i] === "8" || botones[i] === "9" || botones[i] === "0" || botones[i] === "-" || botones[i] === "+" || botones[i] === "/"|| botones[i] === "X"   || botones[i] === "=") {    
+        console.log(boton.value);
+        input.value += boton.value;
+    } 
   });
 }
+
