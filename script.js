2a5500093dab99e0bdfc654ab514544a604eff5a
tiagoo <thurstaltamirano@escuelasproa.edu.ar>
2023-04-18 11:22:05 -0300

    cambios

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 
 
-const botones = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "C", "/", "X", "+", "-","="];
+const botones = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "C", "/", "X", "+", "-", "="];
 let num1 = [];
 let num2 = [];
 let operacionIngresada = false; 
 let input = document.getElementById("result")
 
 
-
+/*
 for (let i = 0; i < botones.length; i++) {
   const boton = document.getElementById(botones[i]);
   boton.addEventListener("click", function () {
@@ -22,4 +22,61 @@ for (let i = 0; i < botones.length; i++) {
     } 
   });
 }
+for (let i = 0; i < botones.length; i++) {
+  const boton = document.getElementById(botones[i]);
+  boton.addEventListener("click", function () {
+    switch (botones[i]) {
+      case 'C':
+        input.value = ""
+      case '1'  || '2':
+        console.log(boton.value);
+        input.value += boton.value;
+        break
+    }
+  })
+}
+*/
+let ultimoBoton = "";
 
+for (let i = 0; i < botones.length; i++) {
+  const boton = document.getElementById(botones[i]);
+  boton.addEventListener("click", function () {
+    switch (botones[i]) {
+      case 'C':
+        input.value = "";
+        console.log('C');
+        break;
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+      case '0':
+        input.value += boton.value;
+        console.log(boton.value);
+        break;
+      case '/':
+      case 'X':
+      case '+':
+      case '-':
+      case '=':
+        if (ultimoBoton !== "" && ultimoBoton.match(/[\/X+\-=]/)) {
+          
+        } else {
+          input.value += boton.value;
+          
+          // Código para realizar operaciones matemáticas
+          console.log(boton.value);
+        }
+        break;
+      default:
+        console.log("Botón no reconocido");
+        break;
+    }
+    ultimoBoton = botones[i];
+  })
+}
